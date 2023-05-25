4efcca4ee52cfb6bccd060f1ba007ae6a545ccd7
felipemontanes <felipe240702@gmail.com>
2023-05-25 13:01:24 -0300

    agrego help y exit

---
+++ builtin_exit.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+
+int builtin_exit (int argc, char ** argv){
+    // termina el shell, admite un parámetro que es el status de retorno. 
+    //Sin ningún parámetro, usa como status de retorno el del último comando ejecutado.
+    return 0;
+}
