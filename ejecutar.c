5c98339cd5c588acd8e66daf1b76f74cea7658b1
gtangari10 <gtangari@correo.um.edu.uy>
2023-05-30 13:07:28 -0300

    Funciona ejecutar

--- ejecutar.c
+++ ejecutar.c
@@ -24,10 +24,10 @@ int ejecutar (int argc, char ** argv){
     //fprintf(stderr, "Entre a ejecutar");
     //fprintf(stderr, "%s", argv[0]);
     for(int i=0; builtin_arr[i].cmd != NULL; i++){
-        fprintf(stderr, "'%s'\n", builtin_arr[i].cmd);
-        fprintf(stderr, "'%s'\n", argv[0]);
+        //fprintf(stderr, "'%s'\n", builtin_arr[i].cmd);
+        //fprintf(stderr, "'%s'\n", argv[0]);
         if(strcmp(argv[0], builtin_arr[i].cmd) == 0){
-            fprintf(stderr, "entre al if");
+            //fprintf(stderr, "entre al if");
             return (builtin_arr[i].func)(argc, argv);
         }
 
