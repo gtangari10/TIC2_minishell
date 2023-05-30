f6934ff190e0244e565a59009a04ce15b1dd13c5
root <root@DELL>
2023-05-30 13:09:42 -0300

    OK

--- builtin_pid.c
+++ builtin_pid.c
@@ -1,11 +1,6 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int builtin_pid (int argc, char ** argv){
     // no lleva parámetros, muestra el process id del shell
     return getpid();
-}
-
-int main(int argc, char *argv){
-    int pid = getpid();
-    printf("%i", pid);
 }
--- wrappers.c
+++ wrappers.c
@@ -11,7 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <error.h>
+#include <error.h>
 #include <errno.h>
 #include <string.h>
 
