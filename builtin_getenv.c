b23f672b6a454c90ae6895c3f21c071f345553f9
root <root@DELL>
2023-05-31 18:26:35 -0300

    Anda el getenv

--- builtin_getenv.c
+++ builtin_getenv.c
@@ -1,8 +1,35 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-int builtin_getenv (int argc, char ** argv){
+char extern **environ;
+
+int all_env_variables();
+int print_variables(int argc, char **argv);
+
+
+int builtin_getenv (int argc, char **argv){
     // Specify the name of the environment variable you want to retrieve
+    if (argc == 1){
+        return all_env_variables();
+    } else {
+        return print_variables(argc, argv);
+    }
+    return 0;
+}
+
+
+int all_env_variables(){
+    char **env = environ;
+
+    // Iterate through each environment variable until NULL is encountered
+    while (*env) {
+        printf("%s\n", *env);
+        env++;
+    }
+    return 0;
+}
+
+int print_variables(int argc, char **argv){
     char *variable_name;
     char *variable_value;
     for (int i = 1; i<argc; i++){
@@ -11,12 +38,9 @@ int builtin_getenv (int argc, char ** argv){
         if (variable_value != NULL){
             printf("%s=%s \n", variable_name, variable_value);
         }else{
-            fprintf(stderr, "No se encuentra ninguna variable de ambiente con nombre: %s", variable_name);
+            fprintf(stderr, "No se encuentra ninguna variable de ambiente con nombre: %s \n", variable_name);
+            return 1;
         }
     }
-
-
-    return 0;
 }
-
 
