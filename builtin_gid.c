4b15d0ab7a9ae629e916277db4b471e72f05d466
gtangari10 <gtangari@correo.um.edu.uy>
2023-05-31 15:09:33 -0300

    Da warnings porque hay un par de cosas que no se usan pero parece andar el gid

--- builtin_gid.c
+++ builtin_gid.c
@@ -1,9 +1,21 @@
 #include <stdio.h> 
 #include <unistd.h>
+#include <string.h>
+#include <grp.h>
 
 int builtin_gid (){
     gid_t git = getgid();
-    fprintf(stderr,"El grupo principal es: %d\n", git);
+    gid_t git_group[100];
+    int number = getgroups(git, git_group);
 
+    struct group * grp = getgrgid(git);
+
+    for(int i=0;git_group[i]!=0;i++){
+        struct group * grp = getgrgid(git_group[i]);
+        if(grp != NULL){
+            fprintf(stderr,"%d(%s)\n", git_group[i], grp->gr_name);
+        }
+    }
+    fprintf(stderr, "\n");
     return 0;
 }
