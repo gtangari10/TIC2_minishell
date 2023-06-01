15a8f5b8e319ae6da7570ee7a99fa859096fc93f
root <root@DELL>
2023-06-01 12:15:02 -0300

    avances con exit

--- builtin_exit.c
+++ builtin_exit.c
@@ -1,8 +1,23 @@
 #include <stdio.h> 
-
+#include <stdlib.h>
+#include "minish.h"
 
 int builtin_exit (int argc, char ** argv){
     // termina el shell, admite un parámetro que es el status de retorno. 
     //Sin ningún parámetro, usa como status de retorno el del último comando ejecutado.
-    return 0;
+    int status = globalstatret;
+
+    if (argc == 1){
+        exit(status);
+    }
+    else {
+        // Se fija que el argumento sea un numero
+        char *endptr;
+        int value = strtol(argv[1], &endptr, 10);
+        if (*endptr == '\0') {
+            exit(value);
+        } else {
+            exit(status);
+        }
+    }
 }
