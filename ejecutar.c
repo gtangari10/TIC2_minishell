f08f718c65b0e4d057b80a27d5c467e73141131e
gtangari10 <gtangari@correo.um.edu.uy>
2023-06-01 13:02:18 -0300

    Avances en externo

--- ejecutar.c
+++ ejecutar.c
@@ -3,27 +3,12 @@
 #include "minish.h"
 
 
-/*
-int ejecutar (int argc, char ** argv){
-    for(int i=0; builtin_arr[i].cmd != NULL; i++){
-        if(strcmp(argv[0], builtin_arr[i].cmd) == 0){
-            return (builtin_arr[i].func)(argc, argv);
-        }
-
-    }
-    //return externo;
-    return 0;
-}
-*/
-
-
 int ejecutar(int argc, char **argv){
     struct builtin_struct * elem = builtin_lookup(argv[0]);
     if(elem != NULL){
         return (elem->func)(argc, argv);
     }
 
-    //return externo;
-    return 0;
+    return externo(argc,argv);
 
 }
--- externo.c
+++ externo.c
@@ -1,5 +1,25 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int externo (int argc, char ** argv){
+    //fprintf(stderr, "Entre a externo\n");
+
+    pid_t pid = fork();
+
+    if(pid == -1){
+        fprintf(stderr, "Error creando proceso hijo\n");
+        return 1;
+    }
+
+    if(pid == 0){
+        execvp(argv[0],argv);
+        exit(1);
+    }
+
+    //Aca el padre tiene que esperar al hijo para matar a los 2
+
     return 0;
 }
