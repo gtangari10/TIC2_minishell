f92196f5014e95c59e19b48fdfd841b84daae4b5
root <root@DELL>
2023-06-06 13:25:45 -0300

    Control C en el externo funciona

--- externo.c
+++ externo.c
@@ -1,6 +1,8 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -8,6 +10,11 @@
 
 int externo (int argc, char ** argv){
     //fprintf(stderr, "Entre a externo\n");
+    struct sigaction sig_old_action;
+    struct sigaction sig_new_action;
+    memset(&sig_new_action, 0, sizeof(sig_old_action));
+    sig_new_action.sa_handler = SIG_IGN;
+    sigaction(SIGINT, &sig_new_action, &sig_old_action);
 
     pid_t pid = fork();
 
@@ -20,14 +27,15 @@ int externo (int argc, char ** argv){
         execvp(argv[0], argv);
         exit(1);
     }
-    /*
-    else{
-        fprintf(stderr, "minish: execvp error \n: No such file or directory\n%s", argv[0]);
-    }
-    */
+
+
+
 
     int status;
     waitpid(pid, &status, 0);
+    
+    sigaction(SIGINT, &sig_old_action, NULL);
+    
     if (status == 256){
         fprintf(stderr, "minish: execvp error \n: No such file or directory\n%s", argv[0]);
         return 2;
