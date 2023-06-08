c075a6cd4fc749cde5e4bdf71692eb16c4f6dee4
root <root@DELL>
2023-06-08 12:52:49 -0300

    Sensual lo que anda el dir

--- builtin_dir.c
+++ builtin_dir.c
@@ -1,11 +1,15 @@
 #include <stdio.h> 
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <dirent.h>
 #include "minish.h"
 
 int print_files_in_directory(char *path);
-char * check_directory(char* path);
+int print_files_with_strstr_in_directory(char* directory, char* string);
+int check_directory(char* directory, char* path);
+void get_new_path(char* argument, char* path);
+extern char* previous_path;
 
 int builtin_dir (int argc, char ** argv){
 
@@ -15,35 +19,39 @@ int builtin_dir (int argc, char ** argv){
         return print_files_in_directory(directory);
     }
     else if(argc == 2){
-        // primero tengo que fijarme si es el nombre de algún otro dir
-        if ((directory = check_directory(argv[1])) != NULL){
-            return print_files_in_directory(directory);
-        } else{
-            directory = getenv("PWD");
-            // sino me fijo que archivos contienen ese str y los imprimo
-            return print_files_with_strstr_in_directory(directory, argv[1]);
-        }
+        char path[MAXCWD];
+        get_new_path(argv[1], path);
+        int result = print_files_in_directory(path);
+
+        if (result == 0) return 0;
+
+        directory = getenv("PWD");
+        // sino me fijo que archivos contienen ese str y los imprimo
+        return print_files_with_strstr_in_directory(directory, argv[1]);
     }
     else if(argc == 3){
-        if ((directory = check_directory(argv[1])) != NULL){
-            return 1;
-        }
-        return print_files_with_strstr_in_directory(directory, argv[2]);
+        char path[MAXCWD];
+        get_new_path(argv[1], path);
+        return print_files_with_strstr_in_directory(path, argv[2]);
     }
     else {
-        fprintf(stderr, "dir acepta uno o dos argumentos, consultar help para ver el uso");
+        fprintf(stderr, "dir acepta uno o dos argumentos, consultar help para ver el uso.\n");
     }
 
     return 0;
 }
 
-char * check_directory(char* path){
-    
+int check_directory(char* directory, char* path){
+    get_new_path(directory, path);
+    DIR *dir = opendir(path);
+    if (dir != NULL) {
+        closedir(dir);
+        return 0; // Valid directory
+    }
+    return 1; // Not a directory or does not exist
 }
 
 int print_files_in_directory(char *path){
-    printf("%s", path);
-    
     DIR * dir = opendir(path);
 
     if (dir == NULL){
@@ -76,10 +84,6 @@ int print_files_with_strstr_in_directory(char *path, char *str){
             result = strstr(entry->d_name, str);
             if (result != NULL){
                 printf("%s\n", entry->d_name);
-<<<<<<< HEAD
-                printf("HOLAAAAAA\n", entry->d_name);
-=======
->>>>>>> 2e026059af1d7881962d809bc5b74403abe29fea
             }
         }
     }
@@ -95,7 +99,7 @@ static void truncate_path(char *path){
     *last_char = '\0';
 }
 
-static void get_new_path(char *argument, char *path){
+void get_new_path(char *argument, char *path){
     //Cambia el path
 
     // Si hay .., saca lo que viene despues de la ultima /
@@ -120,16 +124,3 @@ static void get_new_path(char *argument, char *path){
     }
 }
 
-
-
-
-// dir [texto/directorio] – Simula una ejecución simplificada del comando ls -l. Sin
-// argumentos muestra la lista de archivos del directorio corriente). Con un único
-// argumento, será un texto que:
-// o si corresponde a un directorio existente, muestra la lista de todos los archivos de
-// ese directorio
-// o si no es un directorio, muestra todos los archivos cuyo nombre contenga ese
-// texto.
-// Con dos argumentos, el funcionamiento es la fusión de los dos casos anteriores: el
-// primer argumento debe ser un directorio (si no, es un error) y el segundo es el texto de
-// filtro
